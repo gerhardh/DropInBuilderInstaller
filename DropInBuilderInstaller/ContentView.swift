b015da4477fd45dc0ee1fe7c25d3b76414ca6ceb
Gerhard Heidkamp <gheidkamp@mac.com>
2026-03-25 11:56:34 +0100

    Initial Commit

---
+++ DropInBuilderInstaller/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DropInBuilderInstaller
+//
+//  Created by Gerhard Heidkamp on 2026-03-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DropInBuilderInstaller/DropInBuilderInstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropInBuilderInstallerApp.swift
+//  DropInBuilderInstaller
+//
+//  Created by Gerhard Heidkamp on 2026-03-25.
+//
+
+import SwiftUI
+
+@main
+struct DropInBuilderInstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
